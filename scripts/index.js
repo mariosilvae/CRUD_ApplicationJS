89578bfdedda144d994b57d24db27c2338a2d841
Mario Silva <mario.silvae@outlook.com>
2020-06-13 12:58:07 -0500

    FEAT: Dynamic HTML

--- scripts/index.js
+++ scripts/index.js
@@ -14,3 +14,25 @@ let items = [
 
 //Track Form Submission
 let formStatus = 'add';
+
+//Item HTML
+const generateItem = (item) => {
+  const div = document.createElement('div');
+  div.setAttribute('class', 'list-group-item d-flex justify-content-between align-items-center');
+  div.id = item.id;
+  const span = document.createElement('span');
+  span.setAttribute('class', 'text');
+  span.innerText = item.text;
+  const div2 = document.createElement('div');
+  const editButton = document.createElement('button');
+  editButton.setAttribute('class', 'btn btn-dark editBtn btn-sm mr-1');
+  editButton.innerText = 'Edit';
+  const delButton = document.createElement('button');
+  delButton.setAttribute('class', 'btn btn-danger delBtn btn-sm');
+  delButton.innerText = 'Delete';
+  div2.appendChild(editButton);
+  div2.appendChild(delButton);
+  div.appendChild(span);
+  div.appendChild(div2);
+  return div;
+}
